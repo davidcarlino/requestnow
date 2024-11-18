cdf8564dac7b1f497eec90f3147f94ee38739c4b
davidcarlino <david.carlino20@gmail.com>
2024-11-18 18:08:46 +1100

    removed sidebar internation/pages tabs

--- dashtar/frontend/src/routes/sidebar.js
+++ dashtar/frontend/src/routes/sidebar.js
@@ -75,57 +75,9 @@ const sidebar = [
     icon: FiSettings,
     name: "Settings",
   },
-  {
-    icon: FiGlobe,
-    name: "International",
-    routes: [
-      {
-        path: "/languages",
-        name: "Languages",
-      },
-      {
-        path: "/currencies",
-        name: "Currencies",
-      },
-    ],
-  },
-  {
-    icon: FiTarget,
-    name: "OnlineStore",
-    routes: [
-      {
-        name: "ViewStore",
-        path: "http://localhost:3000",
-        outside: "store",
-      },
 
-      {
-        path: "/store/customization",
-        name: "StoreCustomization",
-      },
-      {
-        path: "/store/store-settings",
-        name: "StoreSettings",
-      },
-    ],
-  },
 
-  {
-    icon: FiSlack,
-    name: "Pages",
-    routes: [
-      // submenu
 
-      {
-        path: "/404",
-        name: "404",
-      },
-      {
-        path: "/coming-soon",
-        name: "Coming Soon",
-      },
-    ],
-  },
 ];
 
 export default sidebar;
